d22347f80f5cf93d6b5c6467f53f1e87294940cc
oskar <oskar.pielech@wp.pl>
2019-12-10 13:48:21 +0100

    new version

--- src/main/java/edu/ib/MainController.java
+++ src/main/java/edu/ib/MainController.java
@@ -1,4 +1,31 @@
 package edu.ib;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
 public class MainController {
+
+    private AtomicClock atomicClock;
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private TextField digitalCLock;
+
+    @FXML
+    private TextField updateCounter;
+
+    @FXML
+    void initialize() {
+        assert digitalCLock != null : "fx:id=\"digitalCLock\" was not injected: check your FXML file 'main.fxml'.";
+        assert updateCounter != null : "fx:id=\"updateCounter\" was not injected: check your FXML file 'main.fxml'.";
+        atomicClock=new AtomicClock();
+        atomicClock.start();
+    }
 }
